998706a4995f77ca4073310015883ecf6f92e403
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-26 23:44:12 +0100

    Updated store folder

---
+++ recipe_search/src/store/index.ts
@@ -0,0 +1,13 @@
+import { createStore } from "vuex/types/index.js";
+import state from "./state";
+import actions from "./actions";
+import mutations from "./mutations";
+import getters from "./getters"
+
+
+const store = createStore({
+    state,
+    actions,
+    mutations,
+    getters
+})
