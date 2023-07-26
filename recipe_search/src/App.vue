76cf74f79dba27fb76421e89d6cde16fb2034a96
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-26 23:31:44 +0100

    Updated App.vue

--- recipe_search/src/App.vue
+++ recipe_search/src/App.vue
@@ -1,10 +1,10 @@
+<template>
+<router-view></router-view>
+</template>
+
 <script setup lang="ts">
 
 </script>
-
-<template>
-<h1 class="bg-blue-500">Hello World :)</h1>
-</template>
 
 <style scoped>
 
