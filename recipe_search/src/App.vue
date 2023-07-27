2aa310d2ce2da13d1f751377020cdbfaa7495c96
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-27 16:46:50 +0100

    Updated styles in App.vue

--- recipe_search/src/App.vue
+++ recipe_search/src/App.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="bg-gray-100 h-full">
         <header class="bg-white shadow h-16 flex justify-between item-center">
-            <router-link :to="{name: 'home'}">
+            <router-link :to="{name: 'home'}" class="inline-flex items-center h-full px-5">
                 Home
             </router-link>
             <div class="flex item-center gap-1">
-                <router-link :to="{name: 'home'}" class="inline-flex items-center px-2 h-full hover:bg-purple-200">
+                <router-link :to="{name: 'home'}" class="inline-flex items-center px-2 h-full transition-colors  hover:bg-purple-100">
                     Search meals
                 </router-link>
-                <router-link :to="{name: 'home'}" class="inline-flex items-center px-2 h-full hover:bg-purple-200">
+                <router-link :to="{name: 'home'}" class="inline-flex items-center px-2 h-full transition-colors  hover:bg-purple-100">
                     by letter
                 </router-link>
-                <router-link :to="{name: 'home'}" class="inline-flex items-center px-2 h-full hover:bg-purple-200">
+                <router-link :to="{name: 'home'}" class="inline-flex items-center px-2 h-full transition-colors  hover:bg-purple-100">
                     by ingredient
                 </router-link>
             </div>
