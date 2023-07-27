046bf308cf8259b9a8a6c711764bc8c639e05d3b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-27 22:29:08 +0100

    Updated logic in SearchByName.vue

--- recipe_search/src/views/SearchByName.vue
+++ recipe_search/src/views/SearchByName.vue
@@ -3,11 +3,20 @@
     <input 
         type="text" 
         class="rounded border-2 border-gray-200 w-full" 
-        placeholder="Search for meals..."/>
+        placeholder="Search for meals..."
+        @change="searchMeals"/>
   </div>
 </template>
 
 <script lang="ts" setup>
+  import axiosClient from '../axiosClient';
+  import { ref } from 'vue';
+
+  const keyword = ref('')
+
+  function searchMeals() {
+    axiosClient.get(`search.php?s=${keyword}`)
+  }
 
 </script>
 
