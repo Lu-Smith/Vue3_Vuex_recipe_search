c1a1e4fad0b7c272236bfe07b175b63ac3ca152c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-27 17:23:37 +0100

    Created DefaultLayout.vue

---
+++ recipe_search/src/components/DefaultLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="bg-gray-100 h-full">
+        <navbar />
+        <main>
+            <router-view></router-view>
+        </main>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import Navbar from './Navbar.vue';
+</script>
--- recipe_search/src/router/index.ts
+++ recipe_search/src/router/index.ts
@@ -3,28 +3,36 @@ import Home from '../views/Home.vue';
 import SearchByName from '../views/SearchByName.vue';
 import SearchByIngredients from '../views/SearchByIngredients.vue';
 import SearchByLetter from '../views/SearchByLetter.vue';
+import DefaultLayout from '../components/DefaultLayout.vue';
 
 const routes = [
     {
         path: '/',
-        name: 'home',
-        component: Home
+        component: DefaultLayout,
+        children: [
+            {
+                path: '/',
+                name: 'home',
+                component: Home
+            },
+            {
+                path: '/by-name/:name?',
+                name: 'byName',
+                component: SearchByName
+            },
+            {
+                path: '/by-ingredients/:ingredients?',
+                name: 'byIngredient',
+                component: SearchByIngredients
+            },
+            {
+                path: '/by-letterletter/:letter?',
+                name: 'byLetter',
+                component: SearchByLetter
+            }
+        ]
     },
-    {
-        path: '/by-name/:name?',
-        name: 'byName',
-        component: SearchByName
-    },
-    {
-        path: '/by-ingredients/:ingredients?',
-        name: 'byIngredient',
-        component: SearchByIngredients
-    },
-    {
-        path: '/by-letterletter/:letter?',
-        name: 'byLetter',
-        component: SearchByLetter
-    }
+   
 ];
 
 const router = createRouter({
