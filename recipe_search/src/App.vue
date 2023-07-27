512e0a429ce3bd01751326941459af501043cef8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-27 16:38:58 +0100

    Updated App.vue

--- recipe_search/src/App.vue
+++ recipe_search/src/App.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="bg-gray-100 h-full">
-        <header class="bg-white shadow h-16">
+        <header class="bg-white shadow h-16 flex justify-between item-center">
             <router-link :to="{name: 'home'}">
                 Home
             </router-link>
-            <div>
-                <router-link :to="{name: 'home'}">
+            <div class="flex item-center gap-1">
+                <router-link :to="{name: 'home'}" class="inline-flex items-center px-2 h-full hover:bg-purple-200">
                     Search meals
                 </router-link>
-                <router-link :to="{name: 'home'}">
+                <router-link :to="{name: 'home'}" class="inline-flex items-center px-2 h-full hover:bg-purple-200">
                     by letter
                 </router-link>
-                <router-link :to="{name: 'home'}">
+                <router-link :to="{name: 'home'}" class="inline-flex items-center px-2 h-full hover:bg-purple-200">
                     by ingredient
                 </router-link>
             </div>
