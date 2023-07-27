80a4d5a3a9965dedc3697210b0b937b5e3652095
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-27 12:02:38 +0100

    Updated styles in Home.vue

--- recipe_search/src/views/Home.vue
+++ recipe_search/src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="flex p-8 justify-center">
+    <div class="flex flex-col p-8 items-center justify-center">
         <input 
         type="text" 
         class="rounded border-2 border-gray-200 w-full" 
         placeholder="Search for meals..."/>
+        <br />
         <div class="flex gap-1">
             <router-link to="/" v-for="(letter, index) of letters" :key={index}>{{ letter }}</router-link>
         </div>
