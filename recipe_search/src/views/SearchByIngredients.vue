4281403f35cb0c0c22a5a51d42ed795b107b2ba5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-02 18:53:43 +0100

    Updated SearchByIngriedients

--- recipe_search/src/views/SearchByIngredients.vue
+++ recipe_search/src/views/SearchByIngredients.vue
@@ -1,13 +1,5 @@
 <template>
   <div>
-  <h2>Search by Ingredients</h2>
-</div>
+    <h2>Search by Ingredients</h2>
+  </div>
 </template>
-
-<script lang="ts" setup>
-
-</script>
-
-<style>
-
-</style>
