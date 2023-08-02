989f44aecc7e00548d10e826d54d8fb78e278ab5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-02 19:53:25 +0100

    Updated SearchByLetter.vue

--- recipe_search/src/views/SearchByLetter.vue
+++ recipe_search/src/views/SearchByLetter.vue
@@ -1,5 +1,19 @@
 <template>
     <h2>Search by Letter</h2>
+    <div class="flex gap-2 mt-2">
+        <router-link 
+        :to="{name: 'byLetter', params: {letter}}" 
+        v-for="letter of letters" 
+        :key="letter">
+            {{ letter }}
+        </router-link>
+    </div>
 </template>
+
+<script lang="ts" setup>
+
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+
+</script>
   
   
