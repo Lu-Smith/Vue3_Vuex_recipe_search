bdd5c1988d32ca5cdfde8d7d05277a279bf1bfd1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-08-02 19:59:23 +0100

    Updated mutations.ts

--- recipe_search/src/store/mutations.ts
+++ recipe_search/src/store/mutations.ts
@@ -3,3 +3,11 @@ import { Meal } from '../types/types';
 export function setSearchedMeals(state: { searchedMeals: Meal[] }, meals: Meal[]) {
     state.searchedMeals = meals
 }
+
+export function setMealsByLetter(state: { mealsByLetter: Meal[] }, meals: Meal[]) {
+    state.mealsByLetter = meals
+}
+
+export function setMealsByIngredients(state: { mealsByIngredients: Meal[] }, meals: Meal[]) {
+    state.mealsByIngredients = meals
+}
